c67d4099262bd78deb5403ed287e1df99e93a48e
adila Ouchen <adilaouchen@hotmail.fr>
2021-05-11 11:41:26 +0200

    connexion au serveur localhost:3000

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Coucou')
+// on importe la librairie de fastify
+import fastify from 'fastify'
+
+// on crée une application en utilisant fastify
+// l'import de notre librairie: on configure
+// fastify pour afficher les logs
+
+const app = fastify({ logger: true })
+
+// cette fonction démarre notre server d'api
+const start = async () => {
+    console.log('lancement de notre serveur...')
+
+    await app.listen(3000)
+
+    console.log('le serveur est lancé, vous pouvez visiter : http://locahost:3000')
+}
+// lancement de la fonction de démarrage
+start()
