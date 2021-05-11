0be28d266b6381dff82cdfe3b8633f9dbc4351d4
adila Ouchen <adilaouchen@hotmail.fr>
2021-05-11 14:05:18 +0200

    Installation Package Falsify-mongodb pour la BDD

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // on importe la librairie de fastify
 import fastify from 'fastify'
+import fastifyMongo from 'fastify-mongodb'
 
 // on crée une application en utilisant fastify
 // l'import de notre librairie: on configure
@@ -7,9 +8,19 @@ import fastify from 'fastify'
 
 const app = fastify({ logger: true })
 
+//On connecte la BDD MongoDB
+app.register(fastifyMongo, {
+    url: 'mongodb+srv://MyTodoApp:MyTodoApp@cluster0.obacx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+})
+
 // on crée une route fastify sur l'URI "/"
 app.get(`/`, async () => {
     return { text: 'Bienvenue' }
+})
+
+// création de la route qui retourne les livres
+app.get('/books', async () => {
+    return []
 })
 
 // cette fonction démarre notre server d'api
