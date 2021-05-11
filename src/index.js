c91e74a7f1c9eff3b225fcf4e58d464e6a1b558e
adila Ouchen <adilaouchen@hotmail.fr>
2021-05-11 16:36:21 +0200

    Création d'1 shéma createBokkSchema pour avoir une structure

--- src/index.js
+++ src/index.js
@@ -33,8 +33,33 @@ app.get('/books', async () => {
     return books
 })
 
-// on crée une route qui nous permettra d'ajouter (de créer) un nouveau livre
-app.post('/books', async (request) => {
+// on déclare un shema qui nous permettra de valider les donnees envoyées dans la request POST/books
+const createBookSchema = {
+    type: 'object',
+    properties: {
+        title: { type: 'string' },
+        description: { type: 'string' },
+        image: { type: 'string' },
+        author: { type: 'string' },
+        price: { type: 'number' },
+        stars: { type: 'number' }
+    },
+    required: [
+        'title',
+        'description',
+        'image',
+        'author',
+        'price',
+        'stars'
+    ]
+}
+
+// on crée une route qui nous permettra d'ajouter (de créer) un nouveau livre et nous lui rattachons un schema
+app.post('/books', {
+    schema: {
+        body: createBookSchema
+    }
+}, async (request) => {
     // nous récupérons ttes les données qu'il y a dans le corps de la requete(cela correspond à notre livre)
     const book = request.body
 
@@ -45,7 +70,7 @@ app.post('/books', async (request) => {
     const result = await collection.insertOne(book)
 
     // on retourne le livre qui a été enregistré dans la BDD
-    return result.ops[0]
+    return result.ops[0]// tous les documents qui ont été enregistrés dans mangodb
 })
 
 // cette fonction démarre notre server d'api
